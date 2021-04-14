30756828a047f1270a630311ad347099d69c807d
Plarpoon <plarpoon.dev@gmail.com>
2021-04-14 22:39:17 +0200

    Add project files.

---
+++ ConfigFile.cs
@@ -0,0 +1,7 @@
+﻿namespace Ruby.Net
+{
+    internal class ConfigFile
+    {
+        public string Token { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Ruby.Net
+{
+    public class RubyNet
+    {
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private DiscordSocketClient _client;
+
+        public static void Main()
+            => new RubyNet().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            var token = JsonConvert.DeserializeObject<ConfigFile>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+    }
+}
