ea8e42da628466eebd1595346d3beb90e70c3ea2
Plarpoon <plarpoon.dev@gmail.com>
2021-05-27 22:22:01 +0200

    init before database

---
+++ Commands/Warcraft/wowSetup.cs
@@ -0,0 +1,21 @@
+﻿//using Discord.Commands;
+//using JetBrains.Annotations;
+//using System.Threading.Tasks;
+
+//namespace RubyNet.Commands.Warcraft
+//{
+//    [UsedImplicitly]
+//    public class WowSetup : ModuleBase
+//    {
+//        [Command("wowsetup")]
+//        [UsedImplicitly]
+//        public async Task WoWSetupCommand()
+//        {
+//            Context.Guild.CreateRoleAsync("")
+//        }
+//    }
+//}
+
+
+
+//  database needed to complete this idea.
